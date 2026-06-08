b55b7f3806e00a4ef4f648b8f9f92e00425620f9
tskorohod <tskorohod@lmteam.by>
2026-06-08 19:18:50 +0300

    added header(desktop)

--- src/components/UI/layouts/header.tsx
+++ src/components/UI/layouts/header.tsx
@@ -2,28 +2,58 @@
 
 import { Button } from "@heroui/react";
 import Image from "next/image";
+import Link from "next/link";
+import { siteConfig } from "@/src/config/site.config";
+import { usePathname } from "next/navigation";
 
 export const Logo = () => {
 	return (
 		<Image
-			src="/food-logo.png"
+			src="/logo.jpg"
 			alt=""
 			width={26} height={26}
 			priority
+			className="rounded-full"
 		/>
 	);
 };
 
 export default function Header() {
+	const pathname = usePathname();
+
+		const getNavItems = () => {
+		return (
+			siteConfig.navItems.map(item => {
+				const isActive = pathname === item.href;
+				return <Link
+					key={item.href}
+					className={`px-2 ${isActive ? "text-blue-800" : "text-foreground"}`}
+					href={item.href}>
+					{item.label}
+				</Link>
+			})
+		);
+	};
 
 	return (
-		<nav className="flex justify-center">
-			<Button className="p-4">
-				Регистрация
-			</Button>
-			<Button>
-				Логин
-			</Button>
-		</nav>
+		<header>
+			<nav className="flex justify-between p-4">
+				<Link href="/" className="flex gap-1 justify-center items-center">
+					<Logo/>
+					<h1 className="font-bold text-inherit">{siteConfig.title}</h1>
+				</Link>
+				<div>
+					{getNavItems()}
+				</div>
+				<div>
+					<Button variant="ghost">
+						Регистрация
+					</Button>
+					<Button variant="ghost">
+						Логин
+					</Button>
+				</div>
+			</nav>
+		</header>
 	);
 }
