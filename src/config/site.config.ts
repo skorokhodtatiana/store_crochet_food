ec78d6985a9d17076dbd1ce7eaef140a28a55130
tskorohod <tskorohod@lmteam.by>
2026-06-08 19:19:08 +0300

    added info

---
+++ src/config/site.config.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+	title: "Natashina pekarnya",
+	navItems: [
+		{href: "/", label: "Главная"},
+		{href: "/about", label: "Обо мне"},
+		{href: "/catalog", label: "Каталог"},
+	]
+}
