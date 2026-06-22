64e0517504b14e431e056016703c1e24194277ea
tskorohod <tskorohod@lmteam.by>
2026-06-22 22:57:33 +0300

    small fix

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
 	navItems: [
 		{href: "/", label: "Главная"},
 		{href: "/about", label: "Обо мне"},
-		{href: "/project", label: "Проекты"},
+		{href: "/projects", label: "Проекты"},
 		{href: "/experience", label: "Опыт"},
 		{href: "/contacts", label: "Контакты"},
 	]
