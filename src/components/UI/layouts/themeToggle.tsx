a3994a3a3991a591d7d749a9cb7734c99fbbe648
tskorohod <tskorohod@lmteam.by>
2026-06-22 22:58:57 +0300

    simplified code

--- src/components/UI/layouts/themeToggle.tsx
+++ src/components/UI/layouts/themeToggle.tsx
@@ -10,23 +10,17 @@ export function ThemeToggle() {
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			setMounted(true)
-		}, 0) // Переносим в очередь макрозадач
+		}, 0)
 
 		return () => clearTimeout(timer)
 	}, [])
-
-	const changeTheme = () => {
-		setTheme(theme === 'dark' ? 'light' : 'dark')
-		console.log("mmmm")
-	}
 
 	if (!mounted) return <div className="w-9 h-9" />
 
 	return (
 		<button
-			// onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-				onClick={changeTheme}
-				className="p-2 rounded-md bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 transition-colors"
+			onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+			className="p-2 rounded-md bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-white transition-colors"
 			>
 			{theme === 'dark' ? '☀️ Светлая' : '🌙 Темная'}
 		</button>
