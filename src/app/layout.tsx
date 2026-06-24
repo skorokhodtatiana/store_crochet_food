3f51b9323486c9b56e1c629403a68382ef749b61
tskorohod <tskorohod@lmteam.by>
2026-06-24 22:05:51 +0300

    changed styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 			suppressHydrationWarning
 			>
-			<body className="min-h-full flex flex-col max-w-5xl mx-auto">
+			<body className="min-h-full flex flex-col max-w-7xl mx-auto">
 				<ThemeProvider
 					attribute="class" 
 					defaultTheme="system" 
