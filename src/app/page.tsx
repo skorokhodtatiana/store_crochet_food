9bbfec45f9d5ae904bcca12a800e240b22ea7412
tskorohod <tskorohod@lmteam.by>
2026-06-26 19:57:01 +0300

    added block scills with animations, added animation bage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
+import { siteConfig } from "../config/site.config";
 
 export default function Home() {
 
 	return (
 		<>
-			<main className="grid grid-cols-2 grid-rows-3 gap-4 justify-between">
+			<main className="grid grid-cols-2 grid-rows-3 gap-4 justify-between overflow-hidden">
 				<div>
+					<div className="mb-4">Доступна для сотрудничества - Минск, Беларусь</div>
 					<h2 className="font-bold text-4xl">
 						Frontend
 						<span className="text-yellow-600">&nbsp;Разработчик</span>
 					</h2>
-					<div className="mt-4">
+					<div className="mt-4 text-2xl text-amber-950">
 						Меня зовут Татьяна и я Frontend разработчик с 3+ годами коммерческого опыта в продуктовой компании - маркетплэйсе.</div>
 					</div>
-				<div className="relative justify-self-end">
+				<div className="relative justify-self-end mb-8">
 					<Image
 						src="/main-photo.JPG"
 						alt=""
@@ -21,11 +23,20 @@ export default function Home() {
 						priority
 						className="rounded-2xl"
 					/>
-					<span className="absolute -top-2 -right-2 text-focus rounded-full bg-accent text-xs font-bold px-2.5 py-1 border-2 border-gray-700">3 года опыта</span>
+					<span className="absolute text-focus rounded-sm bg-accent bottom-0 right-0 text-xs font-bold px-2.5 py-1 border-2 border-gray-700 animate-wiggle">3 года опыта</span>
 				</div>
 				<div className="col-span-2">
-
+					<h4 className="flex self-center justify-center mb-8">Стек технологий</h4>
+					<div className="flex min-w-full shrink-0 justify-around gap-4 animate-marquee" aria-hidden="true">
+						{siteConfig.scills.length && siteConfig.scills.map(item  => (
+							<span className="px-2" key={item.key}>{item.item}</span>
+						))}
+						{siteConfig.scills.length && siteConfig.scills.map(item  => (
+							<span className="px-2" key={item.key}>{item.item}</span>
+						))}
+					</div>
 				</div>
+				
 			</main>
 		</>
 	);
